05921d2105ec8d19b4a9b6e23db2186299886b6b
Shrijith S Menon <71313283+ShrijithSM@users.noreply.github.com>
2025-07-25 23:30:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
-
 import matplotlib.pyplot as plt
 import seaborn as sns
 
